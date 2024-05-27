5d30405d8b2866b281fd0ee5519ecd1666646566
AshaOnye <ashiefortune@gmail.com>
2024-05-27 16:44:15 +0100

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,273 @@
+@extends('layout')
+
+@section('content')
+<div id="page" class="site">
+<div id="content" class="site-content">
+            <div class="page-header flex-middle page-header-about">
+                <div class="container">
+                    <div class="inner flex-middle">
+                        <h1 class="page-title">About Us</h1>
+                        <ul id="breadcrumbs" class="breadcrumbs none-style">
+                            <li><a href="index.html">Home</a></li>
+                            <li class="active">About Us</li>
+                        </ul>    
+                    </div>
+                    <div class="space-110"></div>
+                    <div class="space-60"></div>
+                </div>
+            </div>
+        </div>
+        <section>
+            <div class="container">
+                <div class="space-60 d-none d-md-block"></div>
+                <div class="space-60"></div>
+                <div class="row">
+                    <div class="col-lg-6 mb-5 mb-lg-0 align-self-center">
+                        <div class="about-img">
+                            <img src="/images/image1-about-us.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="offset-xl-1 col-xl-5 col-lg-6 align-self-center">
+                        <div class="ot-heading">
+                            <span class="is_highlight">about company</span>
+                            <h3 class="main-head">Felak Concept Limited</h3>
+                        </div>
+                        <div class="space-20"></div>
+                        <div class="space-4"></div>
+                        <p>Felak Concept Limited (FCL) is an expert consortium offering a range of world class consultancy, training and project management services to private and public sector clients. Established in 2000, FCL has the requisite experience and a team of vastly knowledgeable experts in services and consultancy. The formation of FCL was driven by the need to provide strategic solutions to identified challenges in project management and professional service delivery. Since inception till date, FCL has delivered varied services in offshore and onshore procedures. We have also developed a positive reputation for years with proven excellent consultancy and advisory services.</p>
+                        
+                    </div>
+                </div>
+                <div class="space-60 d-none d-md-block"></div>
+                
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 mb-5 mb-lg-0 align-self-center">
+                     <div class="ot-heading">
+                        <h3 class="main-head">Our Mission</h3>
+                     </div>
+                     <p>To provide specialized services to clients for sustained returns on investment and timely project execution</p>
+                    </div>
+                    <div class="offset-xl-1 col-xl-5 col-lg-6 align-self-center">
+                        <div class="ot-heading">
+                            <h3 class="main-head">Our Vision</h3>
+                         </div>
+                         <p>To position as a global provider of professional and technical consultancy, training and project execution/management services</p>
+                    </div>
+                </div>
+                <div class="space-60 d-none d-md-block"></div>
+                <div class="space-50"></div>
+                <div class="space-5"></div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-6 mb-5 mb-lg-0">
+                        <div class="number-left">
+                            <div class="ot-counter icounter">
+                                <div class="c-number">
+                                    <span class="num" data-to="350" data-time="2000">0</span>
+                                </div>
+                                <h6>Trusted <br>Clients</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6 mb-5 mb-lg-0">
+                        <div class="number-left">
+                            <div class="ot-counter icounter">
+                                <div class="c-number">
+                                    <span class="num" data-to="120" data-time="2000">0</span>
+                                </div>
+                                <h6>Finished  <br>Projects</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="number-left">
+                            <div class="ot-counter icounter">
+                                <div class="c-number">
+                                    <span class="num" data-to="23" data-time="2000">0</span>
+                                </div>
+                                <h6>Years of <br>Experience</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="number-left">
+                            <div class="ot-counter icounter">
+                                <div class="c-number">
+                                    <span class="num" data-to="36" data-time="2000">0</span>
+                                </div>
+                                <h6>Visited <br>Conferences</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="space-60 d-none d-md-block"></div>
+                <div class="space-50"></div>
+                <div class="space-2"></div>
+            </div>
+        </section>
+        <section class="about-work">
+            <div class="container">
+                <div class="space-60 d-none d-md-block"></div>
+                <div class="space-50"></div>
+                <div class="space-5"></div>
+                <div class="row">
+                    <div class="col-xl-8 offset-xl-2 text-center">
+                        <div class="ot-heading">
+                            <span class="is_highlight">Our Core Values</span>
+                            <h3 class="main-head text-light">Guiding Principles that Drive Our Commitment to Excellence</h3>
+                        </div>
+                        <div class="space-20"></div>
+                        <div class="space-3"></div>
+                        
+                    </div>
+                </div>
+                <div class="space-80"></div>
+                <div class="space-4"></div>
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+                        <div class="ot-flip-box">
+                            <div class="inner-box">
+                                <div class="overlay flex-middle">
+                                    <div class="inner">
+                                        <p> We leverage on our years of professional service delivery to initiate strategic ideas for improved client experience.</p>
+                                        
+                                    </div>
+                                </div>
+                                <div class="content-box">
+                                    <div class="number-title">
+                                        <span>01</span><h4>Innovation and Expertise</h4>
+                                    </div>
+                                    <img src="/images/flip-box1.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-5 mb-md-0">
+                        <div class="ot-flip-box">
+                            <div class="inner-box">
+                                <div class="overlay flex-middle">
+                                    <div class="inner">
+                                        <p> We consistently adhere to our aim of excellent service delivery.</p>
+                                        
+                                    </div>
+                                </div>
+                                <div class="content-box">
+                                    <div class="number-title">
+                                        <span>02</span><h4>Trust/Integrity</h4>
+                                    </div>
+                                    <img src="/images/flip-box2.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="ot-flip-box">
+                            <div class="inner-box">
+                                <div class="overlay flex-middle">
+                                    <div class="inner">
+                                        <p> We initiate policies and practices that contribute to the welfare of the society and the health of the natural environment</p>
+                                        
+                                    </div>
+                                </div>
+                                <div class="content-box">
+                                    <div class="number-title">
+                                        <span>03</span><h4>Social Responsiveness</h4>
+                                    </div>
+                                    <img src="/images/flip-box3.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="space-60"></div>
+                <div class="space-6"></div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-md-12 mb-5 mb-lg-0 px-lg-0 text-center">
+                        <img src="/images/image2-about-us.png" alt="">
+                    </div>
+                    <div class="col-lg-6 col-md-12 px-lg-0">
+                        <div class="ot-heading">
+                            
+                            <h3 class="main-head">We Take Your Business to the next Level</h3>
+                        </div>
+                        <div class="space-20"></div>
+                        <div class="space-2"></div>
+                        <p>With our expert project management and procurement services, we streamline your operations and enhance efficiency. Our dedicated team ensures that every project is delivered on time and within budget, leveraging strategic sourcing to maximize value. We provide:</p>
+                        <div class="space-10"></div>
+                        <div class="space-5"></div>
+                        <div class="icon-list d-flex">
+                            <div class="icon-main">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 515.556 515.556"><path d="m0 274.226 176.549 176.886 339.007-338.672-48.67-47.997-290.337 290-128.553-128.552z"></path></svg>
+                            </div>
+                            <div class="content-box">
+                                <h6 class="title-box">Quality Control System</h6>
+                            </div>
+                        </div>
+                        <div class="icon-list d-flex">
+                            <div class="icon-main">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 515.556 515.556"><path d="m0 274.226 176.549 176.886 339.007-338.672-48.67-47.997-290.337 290-128.553-128.552z"></path></svg>
+                            </div>
+                            <div class="content-box">
+                                <h6 class="title-box">100% Satisfaction Guarantee</h6>
+                            </div>
+                        </div>
+                        <div class="icon-list d-flex">
+                            <div class="icon-main">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 515.556 515.556"><path d="m0 274.226 176.549 176.886 339.007-338.672-48.67-47.997-290.337 290-128.553-128.552z"></path></svg>
+                            </div>
+                            <div class="content-box">
+                                <h6 class="title-box">Highly Professional Staff</h6>
+                            </div>
+                        </div>
+                        <div class="icon-list d-flex">
+                            <div class="icon-main">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 515.556 515.556"><path d="m0 274.226 176.549 176.886 339.007-338.672-48.67-47.997-290.337 290-128.553-128.552z"></path></svg>
+                            </div>
+                            <div class="content-box">
+                                <h6 class="title-box">Environmental Sensitivity</h6>
+                            </div>
+                        </div>
+                        <div class="icon-list d-flex">
+                            <div class="icon-main">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 515.556 515.556"><path d="m0 274.226 176.549 176.886 339.007-338.672-48.67-47.997-290.337 290-128.553-128.552z"></path></svg>
+                            </div>
+                            <div class="content-box">
+                                <h6 class="title-box">Personalised solutions</h6>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="space-60 d-lg-none"></div>
+            </div>
+        </section>
+        <section class="cta-main">
+            <div class="container">
+                <div class="space-80"></div>
+                <div class="row align-items-center">
+                    <div class="col-lg-8 mb-5 mb-lg-0">
+                        <h2>Searching for Exceptional Procurement and Consulting Services?</h2>
+                        <p class="text-white">We embrace and celebrate diverse viewpoints to elevate our firm, benefit our clients, and strengthen our team.</p>
+                    </div>
+                    <div class="col-lg-4 text-lg-right">
+                        <div class="ot-button">
+                            <a href="contact.html" class="octf-btn octf-btn-light">get in touch</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="space-80"></div>
+            </div>
+        </section>
+</div>
+@endsection
